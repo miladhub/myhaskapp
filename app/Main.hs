4c6eebf2357b0a97deac0d6bce7744f96c81ed47
Milad Bourhani <milad.bourhani@gmail.com>
2019-06-02 23:19:37 +0200

    transformers

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Lib
 import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Except
 
 main :: IO ()
 main = do
@@ -12,3 +14,81 @@ app = do
   e <- ask
   liftIO $ putStrLn (host e)
   return 42
+
+runFoo :: IO ()
+runFoo = do
+  putStrLn "Running..."
+  let e = Env "myhost" 42
+      s = runReaderT foo $ e
+      ioa = runStateT s []
+  (a, xs) <- ioa
+  putStrLn $ show a
+  return ()
+
+type Foo = ReaderT Env (StateT [String] IO)
+
+foo :: Foo Int
+foo = do
+  (Env host port) <- ask
+  put ["foo" ++ host]
+  liftIO $ putStrLn "fooing"
+  return port
+
+type Baz = ReaderT Env (StateT [String] (ExceptT String IO))
+
+runBaz :: Show a => Baz a -> IO ()
+runBaz baz = do
+  putStrLn "Running..."
+  let e = Env "myhost" 42
+      s = runReaderT baz $ e
+      exc = runStateT s []
+      ioa = runExceptT exc
+  es <- ioa
+  case es of
+    Left err -> putStrLn $ "Error: " ++ err
+    Right (i, xs) -> putStrLn $ "OK: " ++ (show i)
+  return ()
+
+baz :: Baz Int
+baz = do
+  e <- ask
+  put ["baz", host e]
+  return 42
+
+err :: Baz ()
+err =
+  let et = ExceptT $ return $ Left "foo"
+      st = StateT  $ \_ -> et
+      rt = ReaderT $ \_ -> st
+  in rt
+
+type Buz = ReaderT Env (ExceptT String (StateT [String] IO))
+
+runBuz :: Show a => Buz a -> IO ()
+runBuz buz = do
+  putStrLn "Running..."
+  let e = Env "myhost" 42
+      exct = runReaderT buz $ e
+      st = runExceptT exct
+      ei = runStateT st []
+  (e, s) <- ei
+  putStrLn $ "Final state: " ++ show s
+  case e of
+    Left err -> putStrLn $ "Error: " ++ err
+    Right i -> putStrLn $ "OK: " ++ (show i)
+  return ()
+
+buz :: Buz Int
+buz = do
+  e <- ask
+  put ["buz", host e]
+  err'
+  return 42
+
+err' :: Buz ()
+err' = do
+  let et = ExceptT $ return $ Left "foo"
+      rt = ReaderT $ \_ -> et
+  s <- get
+  put $ s ++ ["error..."]
+  rt
